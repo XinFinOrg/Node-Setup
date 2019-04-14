b2d80ff369e90e52364ca376ff9899ea829d6302
digispacedavid <muyiwa.dada@ymail.com>
2019-04-15 01:06:37 +0530

    i18n Update

--- modules/constants.js
+++ modules/constants.js
@@ -1,13 +1,13 @@
 export const InfuraEndpoints = {
   ethereum: {
     http: {
-      Main: 'https://mainnet.infura.io/mist',
+      Main: 'https://testnet.xinfin.network',
       Ropsten: 'https://ropsten.infura.io/mist',
       Rinkeby: 'https://rinkeby.infura.io/mist',
       Kovan: 'https://kovan.infura.io/mist'
     },
     websockets: {
-      Main: 'wss://mainnet.infura.io/ws/mist',
+      Main: 'ws://62.233.65.7:8555',
       Ropsten: 'wss://ropsten.infura.io/ws/mist',
       Rinkeby: 'wss://rinkeby.infura.io/ws/mist',
       Kovan: 'wss://kovan.infura.io/ws/mist'
